7f5d6e61c9b3e10a28e95496762a54bc43bd1741
Usuario755 <425114755@pcpuma.acatlan.unam.mx>
2025-11-21 21:17:14 -0600

    Esto es solo el esqueleto
    De momento solo es la ventana con un botón sin utilidad

---
+++ Proyecto final/Proyecto final/Ahorcado.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Proyecto_final
+{
+    public partial class Ahorcado : Form
+    {
+        public Ahorcado()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
